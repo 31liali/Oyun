6a36058c19f8bf5a3caf5692fec2dd33a787348b
RPDevlet <aliisteneistiyon@gmail.com>
2026-07-13 11:36:16 +0300

    Add Firebase Storage functionality to firebase.js

--- firebase.js
+++ firebase.js
@@ -27,6 +27,14 @@ import {
     serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+import {
+    getStorage,
+    ref,
+    uploadBytes,
+    getDownloadURL,
+    deleteObject
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-storage.js";
+
 const firebaseConfig = {
     apiKey: "AIzaSyASrc6oTod2cmThLCVE9eq67WYXECjUs-s",
     authDomain: "rpdevlet.firebaseapp.com",
@@ -41,10 +49,12 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 export {
     auth,
     db,
+    storage,
 
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
@@ -65,5 +75,10 @@ export {
     getDocs,
 
     addDoc,
-    serverTimestamp
+    serverTimestamp,
+
+    ref,
+    uploadBytes,
+    getDownloadURL,
+    deleteObject
 };
